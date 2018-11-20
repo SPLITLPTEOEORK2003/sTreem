69d01ea394bf15c3546b24f572a92df33df8a3e3
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2018-11-20 15:26:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,36 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '-'
+const adminprefix = "-";
+const developers = ['412275773014016011'];
+
+client.on('ready', () => {
+ console.log("hi"); 
+console.log("bye");
+});
+
+
+
+
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+  } else
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/paaaaaartly");
+  }
+});
+client.login(process.env.TOKEN);
 
